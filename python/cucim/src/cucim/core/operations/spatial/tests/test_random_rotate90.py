1f536ab97a5d7ee51a79097e10f73eaa509ceee5
Shekhar Dwivedi <shekhard@nvidia.com>
2021-09-16 23:53:32 -0700

    test file cleaned up for rand_rotate90

--- python/cucim/src/cucim/core/operations/spatial/tests/test_random_rotate90.py
+++ python/cucim/src/cucim/core/operations/spatial/tests/test_random_rotate90.py
@@ -1,29 +1,4 @@
 import os
-
-import cupy
-import numpy as np
-from PIL import Image
-
-import cucim.core.operations.spatial as spt
-
-img = Image.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "1.png"))
-arr = np.asarray(img)
-arr = np.transpose(arr)
-
-img1 = Image.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "2r.png"))
-arr_o = np.asarray(img1)
-arr_o = np.transpose(arr_o)
-
-cupy_arr = cupy.asarray(arr)
-cupy_output = spt.rand_image_rotate_90(cupy_arr,max_k=1,prob=1.0,spatial_axis=[1,2])
-np_output = cupy.asnumpy(cupy_output)
-
-diff_im = np_output-arr_o
-error_threshold = 1000.01
-
-assert np.sum(diff_im) < error_threshold
-
-import os
 import cupy
 import numpy as np
 from PIL import Image
