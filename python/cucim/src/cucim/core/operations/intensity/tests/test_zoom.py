6a3c0706e3ce384600b4dce99148f3255600d9e1
Chirayu <cgarg@nvidia.com>
2021-10-14 11:14:07 -0700

    Fix param bug to padding kernel, add unit test for zoom out

--- python/cucim/src/cucim/core/operations/intensity/tests/test_zoom.py
+++ python/cucim/src/cucim/core/operations/intensity/tests/test_zoom.py
@@ -16,9 +16,12 @@ def get_input_arr():
     return arr
 
 
-def get_zoomed_data():
+def get_zoomed_data(zoomout=False):
     dirname = os.path.dirname(__file__)
-    img1 = Image.open(os.path.join(os.path.abspath(dirname), "zoomed.png"))
+    if not zoomout:
+        img1 = Image.open(os.path.join(os.path.abspath(dirname), "zoomed.png"))
+    else:
+        img1 = Image.open(os.path.join(os.path.abspath(dirname), "zoomout_padded.png"))
     arr_o = np.asarray(img1)
     arr_o = np.transpose(arr_o)
     return arr_o
@@ -55,6 +58,33 @@ def test_zoom_batchinput():
     zoomed_arr = get_zoomed_data()
     arr_batch = np.stack((arr,) * 8, axis=0)
     np_output = its.zoom(arr_batch, [1.1, 1.1])
+    assert np_output.shape[0] == 8
+
+    for i in range(np_output.shape[0]):
+        assert np.allclose(np_output[i], zoomed_arr)
+
+
+def test_zoomout_numpy_input():
+    arr = get_input_arr()
+    zoomed_arr = get_zoomed_data(True)
+    output = its.zoom(arr, [0.85, 0.85])
+    assert np.allclose(output, zoomed_arr)
+
+
+def test_zoomout_cupy_input():
+    arr = get_input_arr()
+    zoomed_arr = get_zoomed_data(True)
+    cupy_arr = cupy.asarray(arr)
+    cupy_output = its.zoom(cupy_arr, [0.85, 0.85])
+    np_output = cupy.asnumpy(cupy_output)
+    assert np.allclose(np_output, zoomed_arr)
+
+
+def test_zoomout_batchinput():
+    arr = get_input_arr()
+    zoomed_arr = get_zoomed_data(True)
+    arr_batch = np.stack((arr,) * 8, axis=0)
+    np_output = its.zoom(arr_batch, [0.85, 0.85])
     assert np_output.shape[0] == 8
 
     for i in range(np_output.shape[0]):
