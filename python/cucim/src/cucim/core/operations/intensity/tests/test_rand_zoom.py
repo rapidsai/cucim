71933dc43a2df67c36c12a03215d3884f2c06845
Chirayu <cgarg@nvidia.com>
2021-10-14 11:46:27 -0700

    fix style errors

--- python/cucim/src/cucim/core/operations/intensity/tests/test_rand_zoom.py
+++ python/cucim/src/cucim/core/operations/intensity/tests/test_rand_zoom.py
@@ -20,7 +20,8 @@ def get_zoomed_data(zoomout=False):
     if not zoomout:
         img1 = Image.open(os.path.join(os.path.abspath(dirname), "zoomed.png"))
     else:
-        img1 = Image.open(os.path.join(os.path.abspath(dirname), "zoomout_padded.png"))
+        img1 = Image.open(os.path.join(os.path.abspath(dirname),
+                            "zoomout_padded.png"))
     arr_o = np.asarray(img1)
     arr_o = np.transpose(arr_o)
     return arr_o
