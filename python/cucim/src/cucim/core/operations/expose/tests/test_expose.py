1bbc98501b7249b038ebf5196dd96dbc53f0213e
Shekhar Dwivedi <shekhard@nvidia.com>
2021-11-15 09:26:51 -0800

    normalize operation via cuda kernel

--- python/cucim/src/cucim/core/operations/expose/tests/test_expose.py
+++ python/cucim/src/cucim/core/operations/expose/tests/test_expose.py
@@ -2,7 +2,7 @@ from cucim.core.operations.expose.transform import (color_jitter, image_flip,
                                                     image_rotate_90,
                                                     rand_image_flip,
                                                     rand_image_rotate_90,
-                                                    rand_zoom,
+                                                    rand_zoom, normalize_data,
                                                     scale_intensity_range, zoom)
 
 
@@ -11,6 +11,7 @@ def test_exposed_transforms():
     assert image_flip is not None
     assert image_rotate_90 is not None
     assert scale_intensity_range is not None
+    assert normalize_data is not None
     assert zoom is not None
     assert rand_zoom is not None
     assert rand_image_flip is not None
