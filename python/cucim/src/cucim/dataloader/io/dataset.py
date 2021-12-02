024d8214a54748d65e173db1513d456c101350fa
sft-managed <u00ua78mcdbMxLjxr2357@rl-dgx-r12-u36-rapids-dgx107.raplab.nvidia.com>
2021-12-02 17:37:50 +0000

    added iterable and map style datasets

--- python/cucim/src/cucim/dataloader/io/dataset.py
+++ python/cucim/src/cucim/dataloader/io/dataset.py
@@ -1,75 +1,96 @@
+import torch
+import queue
+import threading
 
-import dask
-import numpy as np
-from dask.array.core import new_da_object
 import dask.array as da
+import cupy
+import numpy as np
+import time
+import math
+import os
 
 
-class Dataset:
-    """Universal external-data wrapper for cuCIM DataLoader
+class BatchQueue:
+    def __init__(self, darr, batch_size):
+        self.darr = darr
+        self.batch_size = batch_size
+        self.input_queue = queue.Queue()
+        self.output_queue = queue.Queue()
+        self.worker = threading.Thread(
+            target=self.reader,
+            daemon=True,
+        )      
 
-    The cuCIM `Workflow` and `DataLoader`-related APIs require all
-    external data to be converted to the universal `Dataset` type.  The
-    main purpose of this class is to abstract away the raw format of the
-    data, and to allow our Dataloader class to reliably materialize a
-    `dask_cudf.Array` collection (and/or collection-based iterator)
-    on demand.
-    """
+    def __len__(self):
+        # Number of batches in self.darr
+        return math.ceil(self.darr.shape[0] / self.batch_size)   
 
-    def __init__(
-        self,
-        npy_stack_source,
-        npartitions=None,
-        part_size=None,
-        **kwargs,
-    ):
-    self.npy_stack_source = npy_stack_source
+    def start_worker(self):
+        self.worker.start()
 
-    def to_da(self, indicies):
-        """Returns `dask_cudf.Array` from npy_stack_source
-        """
-        # da = da.from_array()
-        # or
-        da = da.from_npy_stack(self.npy_stack_source) # set chunks?
-        return da
+    def put(self, batch_index):
+        # Add a new batch index to input_queue
+        start_index = batch_index * self.batch_size
+        self.input_queue.put(start_index)
 
-    def to_iter(self,indices=None):
-        """Convert `Dataset` object to a `cudf.Array` iterator.
+    def get(self):
+        # Get a pytorch tensor from output_queue
+        return self.output_queue.get()
 
-        Note that this method will use `to_da` to produce a
-        `dask_cudf.Array`, and materialize a single partition for
-        each iteration.
+    def reader(self):
+        # Function to be executed by the IO worker
+        while True:
+            start_index = self.input_queue.get()
+            self.output_queue.put(
+                torch.as_tensor(
+                    self.darr[
+                        start_index:start_index + self.batch_size
+                    ].compute(scheduler="synchronous")
+                )
+            )
 
-        Parameters
-        ----------
-        indices : list(int); default None
-            A specific list of partition indices to iterate over. If
-            nothing is specified, all partitions will be returned in
-            order (or the shuffled order, if `shuffle=True`).
-        """
+        
+class IterableDaskDataset(torch.utils.data.IterableDataset):
+    def __init__(self, darr, batch_size=32, prefetch=True):
+        super(IterableDaskDataset).__init__()
+        self.darr = darr
+        self.batch_size = batch_size
+        self.prefetch = prefetch
+        self.batch_queue = BatchQueue(self.darr, batch_size=batch_size)
 
-        return ArrayIter(
-            self.to_da(indicies=indicies),
-            indices=indices,
-        )
-
-
-class ArrayIter:
-    def __init__(self, da, indices=None):
-        self.indices = indices
-        self._da = da
-
-    # def __len__(self):
-    #     if self.partition_lens:
-    #         # Use metadata-based partition-size information
-    #         # if/when it is available.  Note that this metadata
-    #         # will not be correct if rows where added or dropped
-    #         # after IO (within Ops).
-    #         return sum(self.partition_lens[i] for i in self.indices)
-    #     if len(self.indices) < self._ddf.npartitions:
-    #         return len(self._ddf.partitions[self.indices])
-    #     return len(self._ddf)
+    def __len__(self):
+        # Total legth of 0th index
+        return self.darr.shape[0]
 
     def __iter__(self):
-        for i in self.indices:
-            yield da[i].compute(scheduler="synchronous")
+        # Simple iteration over batches
+        if self.prefetch:
+            # Let BatchQueue do all the work
+            nbatches = len(self.batch_queue)
+            if nbatches:
+                self.batch_queue.start_worker()
+                self.batch_queue.put(0)
+            for batch_id in range(0, nbatches):
+                if batch_id < nbatches - 1:
+                    # Pre-fetch the next batch
+                    self.batch_queue.put(batch_id + 1)
+                yield self.batch_queue.get()    
+        else:
+            # Iterate over batches directly
+            for start in range(0, len(self), self.batch_size):
+                yield torch.as_tensor(
+                    self.darr[start:start+self.batch_size].compute(scheduler="synchronous")
+                )    
+
+
+
+class MapDaskDataset(torch.utils.data.Dataset):
+    def __init__(self, darr):
+        super(MapDaskDataset).__init__()
+        self.darr = darr
+    
+    def __len__(self):
+        return len(self.darr)
+
+    def __getitem__(self, idx):
+        return self.darr[idx].compute(scheduler="synchronous")
