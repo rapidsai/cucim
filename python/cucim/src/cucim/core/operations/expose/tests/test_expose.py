f8146e668e34c5ad05eb03d6d362e078325db45f
shekhardw <42880237+shekhardw@users.noreply.github.com>
2021-12-03 12:38:40 -0800

    Normalize operation  (#150)
    CUDA based normalization operation integrated into cucim. Normalization supports following types. 
    1- simple range based normalization
    2- Atangent based normalization
    
    closes #151

--- python/cucim/src/cucim/core/operations/expose/tests/test_expose.py
+++ python/cucim/src/cucim/core/operations/expose/tests/test_expose.py
@@ -1,5 +1,6 @@
 from cucim.core.operations.expose.transform import (color_jitter, image_flip,
                                                     image_rotate_90,
+                                                    normalize_data,
                                                     rand_image_flip,
                                                     rand_image_rotate_90,
                                                     rand_zoom,
@@ -11,6 +12,7 @@ def test_exposed_transforms():
     assert image_flip is not None
     assert image_rotate_90 is not None
     assert scale_intensity_range is not None
+    assert normalize_data is not None
     assert zoom is not None
     assert rand_zoom is not None
     assert rand_image_flip is not None
