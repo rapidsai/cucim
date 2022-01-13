b667496195b33ce397d9cae360e598164fe7eab8
Anas Zainul Abidin <aabidin@nvidia.com>
2022-01-13 23:28:45 +0000

    Parmeterizing filters benchmark

--- benchmarks/skimage/cucim_filters_bench.py
+++ benchmarks/skimage/cucim_filters_bench.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import argparse
 
 import cucim.skimage
 import cucim.skimage.filters
@@ -10,107 +11,111 @@ import skimage.filters
 
 from _image_bench import ImageBench
 
-pfile = "cucim_filters_results.pickle"
-if os.path.exists(pfile):
-    with open(pfile, "rb") as f:
-        all_results = pickle.load(f)
-else:
-    all_results = pd.DataFrame()
-dtypes = [np.float32]
+def main(args):
 
-for function_name, fixed_kwargs, var_kwargs, allow_color, allow_nd in [
-    # _gabor.py
-    (
-        "gabor",
-        dict(n_stds=3),
-        dict(frequency=[0.075, 0.1, 0.2, 0.3]),
-        False,
-        False,
-    ),
-    # _gaussian.py
-    (
-        "gaussian",
-        dict(truncate=4.0, preserve_range=True),
-        dict(sigma=[0.25, 1, 4]),
-        True,
-        True,
-    ),
-    # _median.py
-    ("median", dict(mode="nearest"), dict(), False, True),
-    # _rank_order.py
-    ("rank_order", dict(), dict(), False, True),
-    # _unsharp_mask.py
-    (
-        "unsharp_mask",
-        dict(),
-        dict(radius=[0.5, 1.0, 2.0, 3.0]),
-        True,
-        True,
-    ),
-    # edges.py
-    ("sobel", dict(), dict(axis=[None, 0, -1]), False, True),
-    ("prewitt", dict(), dict(axis=[None, 0, -1]), False, True),
-    ("scharr", dict(), dict(axis=[None, 0, -1]), False, True),
-    ("roberts", dict(), dict(), False, False),
-    ("roberts_pos_diag", dict(), dict(), False, False),
-    ("roberts_neg_diag", dict(), dict(), False, False),
-    ("farid", dict(), dict(), False, False),
-    ("laplace", dict(ksize=3), dict(), False, True),
-    # lpi_filter.py
-    # TODO: benchmark wiener
-    # ridges.py
-    # TODO: had to set meijering, etc allow_nd to False just due to insufficient GPU memory
-    (
-        "meijering",
-        dict(sigmas=range(1, 10, 2), alpha=None),
-        dict(black_ridges=[True, False], mode=["reflect"]),
-        False,
-        False,
-    ),
-    (
-        "sato",
-        dict(sigmas=range(1, 10, 2)),
-        dict(black_ridges=[True, False], mode=["reflect"]),
-        False,
-        False,
-    ),
-    (
-        "frangi",
-        dict(sigmas=range(1, 10, 2)),
-        dict(black_ridges=[True, False], mode=["reflect"]),
-        False,
-        False,
-    ),
-    (
-        "hessian",
-        dict(sigmas=range(1, 10, 2)),
-        dict(black_ridges=[True, False], mode=["reflect"]),
-        False,
-        False,
-    ),
-    # thresholding.py
-    ("threshold_isodata", dict(), dict(nbins=[64, 256]), False, True),
-    ("threshold_otsu", dict(), dict(nbins=[64, 256]), False, True),
-    ("threshold_yen", dict(), dict(nbins=[64, 256]), False, True),
-    # TODO: threshold_local should support n-dimensional data
-    (
-        "threshold_local",
-        dict(),
-        dict(block_size=[5, 15], method=["gaussian", "mean", "median"]),
-        False,
-        False,
-    ),
-    ("threshold_li", dict(), dict(), False, True),
-    ("threshold_minimum", dict(), dict(nbins=[64, 256]), False, True),
-    ("threshold_mean", dict(), dict(), False, True),
-    ("threshold_triangle", dict(), dict(nbins=[64, 256]), False, True),
-    ("threshold_niblack", dict(), dict(window_size=[7, 15, 65]), False, True),
-    ("threshold_sauvola", dict(), dict(window_size=[7, 15, 65]), False, True),
-    ("apply_hysteresis_threshold", dict(low=0.15, high=0.6), dict(), False, True),
-    ("threshold_multiotsu", dict(), dict(nbins=[64, 256], classes=[3]), False, True),
-]:
+    pfile = "cucim_filters_results.pickle"
+    if os.path.exists(pfile):
+        with open(pfile, "rb") as f:
+            all_results = pickle.load(f)
+    else:
+        all_results = pd.DataFrame()
+    dtypes = [np.float32]
 
-    for shape in [(512, 512), (3840, 2160), (3840, 2160, 3), (192, 192, 192)]:
+    for function_name, fixed_kwargs, var_kwargs, allow_color, allow_nd in [
+        # _gabor.py
+        (
+            "gabor",
+            dict(n_stds=3),
+            dict(frequency=[0.075, 0.1, 0.2, 0.3]),
+            False,
+            False,
+        ),
+        # _gaussian.py
+        (
+            "gaussian",
+            dict(truncate=4.0, preserve_range=True),
+            dict(sigma=[0.25, 1, 4]),
+            True,
+            True,
+        ),
+        # _median.py
+        ("median", dict(mode="nearest"), dict(), False, True),
+        # _rank_order.py
+        ("rank_order", dict(), dict(), False, True),
+        # _unsharp_mask.py
+        (
+            "unsharp_mask",
+            dict(),
+            dict(radius=[0.5, 1.0, 2.0, 3.0]),
+            True,
+            True,
+        ),
+        # edges.py
+        ("sobel", dict(), dict(axis=[None, 0, -1]), False, True),
+        ("prewitt", dict(), dict(axis=[None, 0, -1]), False, True),
+        ("scharr", dict(), dict(axis=[None, 0, -1]), False, True),
+        ("roberts", dict(), dict(), False, False),
+        ("roberts_pos_diag", dict(), dict(), False, False),
+        ("roberts_neg_diag", dict(), dict(), False, False),
+        ("farid", dict(), dict(), False, False),
+        ("laplace", dict(ksize=3), dict(), False, True),
+        # lpi_filter.py
+        # TODO: benchmark wiener
+        # ridges.py
+        # TODO: had to set meijering, etc allow_nd to False just due to insufficient GPU memory
+        (
+            "meijering",
+            dict(sigmas=range(1, 10, 2), alpha=None),
+            dict(black_ridges=[True, False], mode=["reflect"]),
+            False,
+            False,
+        ),
+        (
+            "sato",
+            dict(sigmas=range(1, 10, 2)),
+            dict(black_ridges=[True, False], mode=["reflect"]),
+            False,
+            False,
+        ),
+        (
+            "frangi",
+            dict(sigmas=range(1, 10, 2)),
+            dict(black_ridges=[True, False], mode=["reflect"]),
+            False,
+            False,
+        ),
+        (
+            "hessian",
+            dict(sigmas=range(1, 10, 2)),
+            dict(black_ridges=[True, False], mode=["reflect"]),
+            False,
+            False,
+        ),
+        # thresholding.py
+        ("threshold_isodata", dict(), dict(nbins=[64, 256]), False, True),
+        ("threshold_otsu", dict(), dict(nbins=[64, 256]), False, True),
+        ("threshold_yen", dict(), dict(nbins=[64, 256]), False, True),
+        # TODO: threshold_local should support n-dimensional data
+        (
+            "threshold_local",
+            dict(),
+            dict(block_size=[5, 15], method=["gaussian", "mean", "median"]),
+            False,
+            False,
+        ),
+        ("threshold_li", dict(), dict(), False, True),
+        ("threshold_minimum", dict(), dict(nbins=[64, 256]), False, True),
+        ("threshold_mean", dict(), dict(), False, True),
+        ("threshold_triangle", dict(), dict(nbins=[64, 256]), False, True),
+        ("threshold_niblack", dict(), dict(window_size=[7, 15, 65]), False, True),
+        ("threshold_sauvola", dict(), dict(window_size=[7, 15, 65]), False, True),
+        ("apply_hysteresis_threshold", dict(low=0.15, high=0.6), dict(), False, True),
+        ("threshold_multiotsu", dict(), dict(nbins=[64, 256], classes=[3]), False, True),
+    ]:
+
+        shape = tuple(args.img_size)
+
+        # for shape in [(512, 512), (3840, 2160), (3840, 2160, 3), (192, 192, 192)]:
 
         ndim = len(shape)
         if not allow_nd:
@@ -150,8 +155,16 @@ for function_name, fixed_kwargs, var_kwargs, allow_color, allow_nd in [
         results = B.run_benchmark(duration=1)
         all_results = all_results.append(results["full"])
 
-fbase = os.path.splitext(pfile)[0]
-all_results.to_csv(fbase + ".csv")
-all_results.to_pickle(pfile)
-with open(fbase + ".md", "wt") as f:
-    f.write(all_results.to_markdown())
+    fbase = os.path.splitext(pfile)[0]
+    all_results.to_csv(fbase + ".csv")
+    all_results.to_pickle(pfile)
+    with open(fbase + ".md", "wt") as f:
+        f.write(all_results.to_markdown())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Benchmarking cuCIM Filters')
+    parser.add_argument('-i','--img_size', type=int, nargs="+", help='Size of input image', required=True)
+    args = parser.parse_args()
+    # print(args.img_size)
+    main(args)
