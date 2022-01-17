404e6001d05cbc74bde7521b010d15555905b743
Gregory Lee <grlee77@gmail.com>
2022-01-16 20:50:39 -0500

    ENH: add mode and cval to canny
    default mode is now 'constant'
    
    refactor code style as done upstream

--- python/cucim/src/cucim/skimage/feature/_canny.py
+++ python/cucim/src/cucim/skimage/feature/_canny.py
@@ -28,20 +28,39 @@ def _fused_comparison(w, c1, c2, m):
     return c2 * w + c1 * (1.0 - w) <= m
 
 
-def smooth_with_function_and_mask(image, function, mask):
-    """Smooth an image with a linear function, ignoring masked pixels.
+def _preprocess(image, mask, sigma, mode, cval):
+    """Generate a smoothed image and an eroded mask.
+
+    The image is smoothed using a gaussian filter ignoring masked
+    pixels and the mask is eroded.
 
     Parameters
     ----------
     image : array
-        Image you want to smooth.
-    function : callable
-        A function that does image smoothing.
+        Image to be smoothed.
     mask : array
         Mask with 1's for significant pixels, 0's for masked pixels.
+    sigma : scalar or sequence of scalars
+        Standard deviation for Gaussian kernel. The standard
+        deviations of the Gaussian filter are given for each axis as a
+        sequence, or as a single number, in which case it is equal for
+        all axes.
+    mode : str, {'reflect', 'constant', 'nearest', 'mirror', 'wrap'}
+        The ``mode`` parameter determines how the array borders are
+        handled, where ``cval`` is the value when mode is equal to
+        'constant'.
+    cval : float, optional
+        Value to fill past edges of input if `mode` is 'constant'.
+
+    Returns
+    -------
+    smoothed_image : ndarray
+        The smoothed array
+    eroded_mask : ndarray
+        The eroded mask.
 
     Notes
-    ------
+    -----
     This function calculates the fractional contribution of masked pixels
     by applying the function to the mask (which gets you the fraction of
     the pixel data that's due to significant points). We then mask the image
@@ -50,16 +69,147 @@ def smooth_with_function_and_mask(image, function, mask):
     on the mask to recover the effect of smoothing from just the significant
     pixels.
     """
-    bleed_over = function(mask.astype(cp.float32))
-    masked_image = cp.zeros(image.shape, image.dtype)
+
+    gaussian_kwargs = dict(sigma=sigma, mode=mode, cval=cval,
+                           preserve_range=False)
+    if mask is None:
+        # Smooth the masked image
+        smoothed_image = gaussian(image, **gaussian_kwargs)
+        # mask that is ones everywhere except the borders
+        eroded_mask = cp.ones(image.shape, dtype=bool)
+        eroded_mask[:1, :] = 0
+        eroded_mask[-1:, :] = 0
+        eroded_mask[:, :1] = 0
+        eroded_mask[:, -1:] = 0
+        return smoothed_image, eroded_mask
+
+    masked_image = cp.zeros_like(image)
     masked_image[mask] = image[mask]
-    smoothed_image = function(masked_image)
-    output_image = smoothed_image / (bleed_over + cp.finfo(cp.float32).eps)
-    return output_image
+
+    # Compute the fractional contribution of masked pixels by applying
+    # the function to the mask (which gets you the fraction of the
+    # pixel data that's due to significant points)
+    bleed_over = gaussian(mask.astype(cp.float32), **gaussian_kwargs)
+    bleed_over += cp.finfo(cp.float32).eps
+
+    # Smooth the masked image
+    smoothed_image = gaussian(masked_image, **gaussian_kwargs)
+
+    # Lower the result by the bleed-over fraction, so you can
+    # recalibrate by dividing by the function on the mask to recover
+    # the effect of smoothing from just the significant pixels.
+    smoothed_image /= bleed_over
+
+    #
+    # Make the eroded mask. Setting the border value to zero will wipe
+    # out the image edges for us.
+    #
+    s = generate_binary_structure(2, 2)
+    eroded_mask = binary_erosion(mask, s, border_value=0)
+
+    return smoothed_image, eroded_mask
+
+
+def _set_local_maxima(magnitude, pts, w_num, w_denum, row_slices,
+                      col_slices, out):
+    """Get the magnitudes shifted left to make a matrix of the points to
+    the right of pts. Similarly, shift left and down to get the points
+    to the top right of pts.
+    """
+    r_0, r_1, r_2, r_3 = row_slices
+    c_0, c_1, c_2, c_3 = col_slices
+    c1 = magnitude[r_0, c_0][pts[r_1, c_1]]
+    c2 = magnitude[r_2, c_2][pts[r_3, c_3]]
+    m = magnitude[pts]
+    w = w_num[pts] / w_denum[pts]
+    c_plus = c2 * w + c1 * (1 - w) <= m
+    c1 = magnitude[r_1, c_1][pts[r_0, c_0]]
+    c2 = magnitude[r_3, c_3][pts[r_2, c_2]]
+    c_minus = c2 * w + c1 * (1 - w) <= m
+    out[pts] = c_plus & c_minus
+
+    return out
+
+
+def _get_local_maxima(isobel, jsobel, magnitude, eroded_mask):
+    """Edge thinning by non-maximum suppression.
+
+    Finds the normal to the edge at each point using the arctangent of the
+    ratio of the Y sobel over the X sobel - pragmatically, we can
+    look at the signs of X and Y and the relative magnitude of X vs Y
+    to sort the points into 4 categories: horizontal, vertical,
+    diagonal and antidiagonal.
+
+    Look in the normal and reverse directions to see if the values
+    in either of those directions are greater than the point in question.
+    Use interpolation (via _set_local_maxima) to get a mix of points
+    instead of picking the one that's the closest to the normal.
+    """
+    abs_isobel = cp.abs(isobel)
+    abs_jsobel = cp.abs(jsobel)
+    eroded_mask = eroded_mask & (magnitude > 0)
+
+    # Normals' orientations
+    is_horizontal = eroded_mask & (abs_isobel >= abs_jsobel)
+    is_vertical = eroded_mask & (abs_isobel <= abs_jsobel)
+    is_up = (isobel >= 0)
+    is_down = (isobel <= 0)
+    is_right = (jsobel >= 0)
+    is_left = (jsobel <= 0)
+
+    # TODO: implement custom kernel to compute local maxima
+
+    #
+    # --------- Find local maxima --------------
+    #
+    # Assign each point to have a normal of 0-45 degrees, 45-90 degrees,
+    # 90-135 degrees and 135-180 degrees.
+    #
+    local_maxima = cp.zeros(magnitude.shape, bool)
+    # ----- 0 to 45 degrees ------
+    pts_plus = is_up & is_right
+    pts_minus = is_down & is_left
+    pts = ((pts_plus | pts_minus) & is_horizontal)
+    local_maxima = _set_local_maxima(
+        magnitude, pts, abs_jsobel, abs_isobel,
+        [slice(1, None), slice(-1), slice(1, None), slice(-1)],
+        [slice(None), slice(None), slice(1, None), slice(-1)],
+        local_maxima)
+    # ----- 45 to 90 degrees ------
+    # Mix diagonal and vertical
+    #
+    pts = ((pts_plus | pts_minus) & is_vertical)
+    local_maxima = _set_local_maxima(
+        magnitude, pts, abs_isobel, abs_jsobel,
+        [slice(None), slice(None), slice(1, None), slice(-1)],
+        [slice(1, None), slice(-1), slice(1, None), slice(-1)],
+        local_maxima)
+    # ----- 90 to 135 degrees ------
+    # Mix anti-diagonal and vertical
+    #
+    pts_plus = is_down & is_right
+    pts_minus = is_up & is_left
+    pts = ((pts_plus | pts_minus) & is_vertical)
+    local_maxima = _set_local_maxima(
+        magnitude, pts, abs_isobel, abs_jsobel,
+        [slice(None), slice(None), slice(-1), slice(1, None)],
+        [slice(1, None), slice(-1), slice(1, None), slice(-1)],
+        local_maxima)
+    # ----- 135 to 180 degrees ------
+    # Mix anti-diagonal and anti-horizontal
+    #
+    pts = ((pts_plus | pts_minus) & is_horizontal)
+    local_maxima = _set_local_maxima(
+        magnitude, pts, abs_jsobel, abs_isobel,
+        [slice(-1), slice(1, None), slice(-1), slice(1, None)],
+        [slice(None), slice(None), slice(1, None), slice(-1)],
+        local_maxima)
+
+    return local_maxima
 
 
 def canny(image, sigma=1., low_threshold=None, high_threshold=None, mask=None,
-          use_quantiles=False):
+          use_quantiles=False, *, mode='constant', cval=0.0):
     """Edge filter an image using the Canny algorithm.
 
     Parameters
@@ -77,9 +227,16 @@ def canny(image, sigma=1., low_threshold=None, high_threshold=None, mask=None,
     mask : array, dtype=bool, optional
         Mask to limit the application of Canny to a certain area.
     use_quantiles : bool, optional
-        If True then treat low_threshold and high_threshold as quantiles of the
-        edge magnitude image, rather than absolute edge magnitude values. If
-        True, then the thresholds must be in the range [0, 1].
+        If ``True`` then treat low_threshold and high_threshold as
+        quantiles of the edge magnitude image, rather than absolute
+        edge magnitude values. If ``True`` then the thresholds must be
+        in the range [0, 1].
+    mode : str, {'reflect', 'constant', 'nearest', 'mirror', 'wrap'}
+        The ``mode`` parameter determines how the array borders are
+        handled during Gaussian filtering, where ``cval`` is the value when
+        mode is equal to 'constant'.
+    cval : float, optional
+        Value to fill past edges of input if `mode` is 'constant'.
 
     Returns
     -------
@@ -113,7 +270,7 @@ def canny(image, sigma=1., low_threshold=None, high_threshold=None, mask=None,
       low threshold that is 8-connected to a labeled point as an edge.
 
     References
-    -----------
+    ----------
     .. [1] Canny, J., A Computational Approach To Edge Detection, IEEE Trans.
            Pattern Analysis and Machine Intelligence, 8:679-714, 1986
            :DOI:`10.1109/TPAMI.1986.4767851`
@@ -162,7 +319,7 @@ def canny(image, sigma=1., low_threshold=None, high_threshold=None, mask=None,
     # that is "infected" by the masked point, so it's enough to erode the
     # mask by one and then mask the output. We also mask out the border points
     # because who knows what lies beyond the edge of the image?
-    #
+
     check_nD(image, 2)
     dtype_max = dtype_limits(image, clip_negative=False)[1]
 
@@ -172,7 +329,7 @@ def canny(image, sigma=1., low_threshold=None, high_threshold=None, mask=None,
         if not (0.0 <= low_threshold <= 1.0):
             raise ValueError("Quantile thresholds must be between 0 and 1.")
     else:
-        low_threshold = low_threshold / dtype_max
+        low_threshold /= dtype_max
 
     if high_threshold is None:
         high_threshold = 0.2
@@ -180,135 +337,40 @@ def canny(image, sigma=1., low_threshold=None, high_threshold=None, mask=None,
         if not (0.0 <= high_threshold <= 1.0):
             raise ValueError("Quantile thresholds must be between 0 and 1.")
     else:
-        high_threshold = high_threshold / dtype_max
+        high_threshold /= dtype_max
 
-    _gaussian = functools.partial(gaussian, sigma=sigma)
+    if high_threshold < low_threshold:
+        raise ValueError("low_threshold should be lower then high_threshold")
 
-    def fsmooth(x, mode='constant'):
-        return img_as_float(_gaussian(x, mode=mode))
+    # Image filtering
+    smoothed, eroded_mask = _preprocess(image, mask, sigma, mode, cval)
 
-    if mask is None:
-        smoothed = fsmooth(image, mode='reflect')
-        # mask that is ones everywhere except the borders
-        eroded_mask = cp.ones(image.shape, dtype=bool)
-        eroded_mask[:1, :] = 0
-        eroded_mask[-1:, :] = 0
-        eroded_mask[:, :1] = 0
-        eroded_mask[:, -1:] = 0
-    else:
-        smoothed = smooth_with_function_and_mask(image, fsmooth, mask)
-        #
-        # Make the eroded mask. Setting the border value to zero will wipe
-        # out the image edges for us.
-        #
-        s = generate_binary_structure(2, 2)
-        eroded_mask = binary_erosion(mask, s, border_value=0)
-
+    # Gradient magnitude estimation
     jsobel = ndi.sobel(smoothed, axis=1)
     isobel = ndi.sobel(smoothed, axis=0)
-    abs_isobel = cp.abs(isobel)
-    abs_jsobel = cp.abs(jsobel)
     magnitude = cp.hypot(isobel, jsobel)
-    eroded_mask = eroded_mask & (magnitude > 0)
-    # TODO: implement custom kernel to compute local maxima
 
-    #
-    # --------- Find local maxima --------------
-    #
-    # Assign each point to have a normal of 0-45 degrees, 45-90 degrees,
-    # 90-135 degrees and 135-180 degrees.
-    #
-    local_maxima = cp.zeros(image.shape, bool)
-
-    isobel_gt_0 = isobel >= 0
-    jsobel_gt_0 = jsobel >= 0
-    isobel_lt_0 = isobel <= 0
-    jsobel_lt_0 = jsobel <= 0
-    abs_isobel_lt_jsobel = abs_isobel <= abs_jsobel
-    abs_isobel_gt_jsobel = abs_isobel >= abs_jsobel
-
-    # ----- 0 to 45 degrees ------
-    pts_plus = isobel_gt_0 & jsobel_gt_0
-    pts_minus = isobel_lt_0 & jsobel_lt_0
-    pts_tmp = (pts_plus | pts_minus) & eroded_mask
-    pts = pts_tmp & abs_isobel_gt_jsobel
-    # Get the magnitudes shifted left to make a matrix of the points to the
-    # right of pts. Similarly, shift left and down to get the points to the
-    # top right of pts.
-
-    c1 = magnitude[1:, :][pts[:-1, :]]
-    c2 = magnitude[1:, 1:][pts[:-1, :-1]]
-    m = magnitude[pts]
-    w = abs_jsobel[pts] / abs_isobel[pts]
-    c_plus = _fused_comparison(w, c1, c2, m)
-    c1 = magnitude[:-1, :][pts[1:, :]]
-    c2 = magnitude[:-1, :-1][pts[1:, 1:]]
-    c_minus = _fused_comparison(w, c1, c2, m)
-    local_maxima[pts] = c_plus & c_minus
-    # ----- 45 to 90 degrees ------
-    # Mix diagonal and vertical
-    #
-    pts = pts_tmp & abs_isobel_lt_jsobel
-    c1 = magnitude[:, 1:][pts[:, :-1]]
-    c2 = magnitude[1:, 1:][pts[:-1, :-1]]
-    m = magnitude[pts]
-    w = abs_isobel[pts] / abs_jsobel[pts]
-    c_plus = _fused_comparison(w, c1, c2, m)
-    c1 = magnitude[:, :-1][pts[:, 1:]]
-    c2 = magnitude[:-1, :-1][pts[1:, 1:]]
-    c_minus = _fused_comparison(w, c1, c2, m)
-    local_maxima[pts] = c_plus & c_minus
-    # ----- 90 to 135 degrees ------
-    # Mix anti-diagonal and vertical
-    #
-    pts_plus = isobel_lt_0 & jsobel_gt_0
-    pts_minus = isobel_gt_0 & jsobel_lt_0
-    pts_tmp = (pts_plus | pts_minus) & eroded_mask
-    pts = pts_tmp & abs_isobel_lt_jsobel
-    c1a = magnitude[:, 1:][pts[:, :-1]]
-    c2a = magnitude[:-1, 1:][pts[1:, :-1]]
-    m = magnitude[pts]
-    w = abs_isobel[pts] / abs_jsobel[pts]
-    c_plus = _fused_comparison(w, c1a, c2a, m)
-    c1 = magnitude[:, :-1][pts[:, 1:]]
-    c2 = magnitude[1:, :-1][pts[:-1, 1:]]
-    c_minus = _fused_comparison(w, c1, c2, m)
-    local_maxima[pts] = c_plus & c_minus
-    # ----- 135 to 180 degrees ------
-    # Mix anti-diagonal and anti-horizontal
-    #
-    pts = pts_tmp & abs_isobel_gt_jsobel
-    c1 = magnitude[:-1, :][pts[1:, :]]
-    c2 = magnitude[:-1, 1:][pts[1:, :-1]]
-    m = magnitude[pts]
-    w = abs_jsobel[pts] / abs_isobel[pts]
-    c_plus = _fused_comparison(w, c1, c2, m)
-    c1 = magnitude[1:, :][pts[:-1, :]]
-    c2 = magnitude[1:, :-1][pts[:-1, 1:]]
-    c_minus = _fused_comparison(w, c1, c2, m)
-    local_maxima[pts] = c_plus & c_minus
-
-    #
-    # ---- If use_quantiles is set then calculate the thresholds to use
-    #
     if use_quantiles:
-        high_threshold = cp.percentile(magnitude, 100.0 * high_threshold)
-        low_threshold = cp.percentile(magnitude, 100.0 * low_threshold)
+        low_threshold, high_threshold = cp.percentile(
+            magnitude, [100.0 * low_threshold, 100.0 * high_threshold]
+        )
 
-    #
-    # ---- Create two masks at the two thresholds.
-    #
-    high_mask = local_maxima & (magnitude >= high_threshold)
+    # Non-maximum suppression
+    local_maxima = _get_local_maxima(isobel, jsobel, magnitude, eroded_mask)
+
+    # Double thresholding and edge tracking
     low_mask = local_maxima & (magnitude >= low_threshold)
 
     #
     # Segment the low-mask, then only keep low-segments that have
     # some high_mask component in them
     #
-    labels, count = ndi.label(low_mask, structure=cp.ones((3, 3), bool))
+    strel = cp.ones((3, 3), bool)
+    labels, count = ndi.label(low_mask, structure=strel)
     if count == 0:
         return low_mask
 
+    high_mask = local_maxima & (magnitude >= high_threshold)
     nonzero_sums = cp.unique(labels[high_mask])
     good_label = cp.zeros((count + 1,), bool)
     good_label[nonzero_sums] = True
