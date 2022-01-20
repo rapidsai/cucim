68c1fe5b06d6034e0f58f6a307ec3184581049f9
Gregory Lee <grlee77@gmail.com>
2022-01-20 12:23:51 -0500

    revert addition of conftest.py and option for error on warning

--- python/cucim/src/cucim/skimage/conftest.py
+++
@@ -1,9 +0,0 @@
-from cucim.skimage._shared.testing import setup_test, teardown_test
-
-
-def pytest_runtest_setup(item):
-    setup_test()
-
-
-def pytest_runtest_teardown(item):
-    teardown_test()
