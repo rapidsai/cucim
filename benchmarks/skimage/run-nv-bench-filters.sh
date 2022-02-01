4ca522094be34ba1e224b88878e01e4c80290f70
Anas Zainul Abidin <aabidin@nvidia.com>
2022-02-01 14:55:11 -0500

    Parmeterizing filters benchmark 4

--- benchmarks/skimage/run-nv-bench-filters.sh
+++ benchmarks/skimage/run-nv-bench-filters.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 param_shape=(512,512 3840,2160 3840,2160,3 192,192,192)
-param_filt=('gabor gaussian median rank_order unsharp_mask sobel prewitt scharr roberts roberts_pos_diag roberts_neg_diag farid laplace meijering sato frangi hessian threshold_isodata threshold_otsu threshold_yen threshold_local threshold_li threshold_minimum threshold_mean threshold_triangle threshold_niblack threshold_sauvola apply_hysteresis_threshold threshold_multiotsu')
+param_filt=(gabor gaussian median rank_order unsharp_mask sobel prewitt scharr roberts roberts_pos_diag roberts_neg_diag farid laplace meijering sato frangi hessian threshold_isodata threshold_otsu threshold_yen threshold_local threshold_li threshold_minimum threshold_mean threshold_triangle threshold_niblack threshold_sauvola apply_hysteresis_threshold threshold_multiotsu)
 param_dt=('fp64' 'fp32' 'fp16')
 for shape in "${param_shape[@]}"; do
     for filt in "${param_filt[@]}"; do
