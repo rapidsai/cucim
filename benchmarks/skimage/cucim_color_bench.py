8b4c590454d2fa37828d80f1ed171afa8c268b5b
Gregory Lee <grlee77@gmail.com>
2022-06-29 10:54:25 -0400

    refactor color benchmarks for command-line use
    follows same approach taken previously for filters and features benchmarks

--- benchmarks/skimage/cucim_color_bench.py
+++ benchmarks/skimage/cucim_color_bench.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import pickle
 
@@ -47,6 +48,7 @@ class LabelBench(ImageBench):
         index_str=None,
         module_cpu=scipy.ndimage,
         module_gpu=cupyx.scipy.ndimage,
+        run_cpu=True,
     ):
         self.contiguous_labels = contiguous_labels
         super().__init__(
@@ -58,6 +60,7 @@ class LabelBench(ImageBench):
             index_str=index_str,
             module_cpu=module_cpu,
             module_gpu=module_gpu,
+            run_cpu=run_cpu,
         )
 
     def set_args(self, dtype):
@@ -70,14 +73,12 @@ class LabelBench(ImageBench):
             ],
             dtype=int,
         )
-        tiling = tuple(s // a_s for s, a_s in zip(shape, a.shape))
+        tiling = tuple(s // a_s for s, a_s in zip(self.shape, a.shape))
         if self.contiguous_labels:
             label = np.kron(a, np.ones(tiling, dtype=a.dtype))
         else:
             label = np.tile(a, tiling)
         labeld = cp.asarray(label)
-        if self.shape[-1] != 3:
-            raise ValueError("shape must be 3 on the last axis")
         imaged = cupy.testing.shaped_random(labeld.shape, xp=cp, dtype=dtype, scale=1.0)
         image = cp.asnumpy(imaged)
         self.args_cpu = (
@@ -90,96 +91,131 @@ class LabelBench(ImageBench):
         )
 
 
-pfile = "cucim_color_results.pickle"
-if os.path.exists(pfile):
-    with open(pfile, "rb") as f:
-        all_results = pickle.load(f)
-else:
-    all_results = pd.DataFrame()
-dtypes = [np.float32]
-all_colorspaces = True
+def main(args):
 
-for shape in [(256, 256, 3), (3840, 2160, 3), (192, 192, 192, 3)]:
-    ndim = len(shape)
-
-    if all_colorspaces:
-        color_spaces = ["RGB", "HSV", "RGB CIE", "XYZ", "YUV", "YIQ", "YPbPr", "YCbCr", "YDbDr"]
+    pfile = "cucim_color_results.pickle"
+    if os.path.exists(pfile):
+        with open(pfile, "rb") as f:
+            all_results = pickle.load(f)
     else:
-        color_spaces = ["RGB", "HSV", "YUV", "XYZ"]
-    for fromspace in color_spaces:
-        for tospace in color_spaces:
-            if fromspace == tospace:
-                continue
+        all_results = pd.DataFrame()
 
-            B = ColorBench(
-                function_name="convert_colorspace",
-                shape=shape,
+    dtype_dict = {'fp64': np.float64, 'fp32': np.float32, 'fp16': np.float16}
+    if args.dtype in dtype_dict:
+        dtypes = [dtype_dict[args.dtype]]
+    else:
+        dtypes = [np.dtype(args.dtype)]
+
+    all_colorspaces = False
+
+    # image sizes/shapes
+    shape = tuple(list(map(int,(args.img_size.split(',')))))
+    ndim = len(shape)
+    run_cpu = not args.no_cpu
+
+    for function_name in ['convert_colorspace', 'rgba2rgb', 'label2rgb', 'rgb2hed', 'hed2rgb', 'lab2lch', 'lch2lab', 'xyz2lab', 'lab2xyz']:
+
+        if function_name != args.func_name:
+            continue
+
+        if function_name == 'convert_colorspace':
+            if all_colorspaces:
+                color_spaces = ["RGB", "HSV", "RGB CIE", "XYZ", "YUV", "YIQ", "YPbPr", "YCbCr", "YDbDr"]
+            else:
+                color_spaces = ["RGB", "HSV", "YUV", "XYZ"]
+            for fromspace in color_spaces:
+                for tospace in color_spaces:
+                    if fromspace == tospace:
+                        continue
+
+                    B = ColorBench(
+                        function_name="convert_colorspace",
+                        shape=shape + (3,),
+                        dtypes=dtypes,
+                        fixed_kwargs=dict(fromspace=fromspace, tospace=tospace),
+                        var_kwargs={},
+                        index_str=f"{fromspace.lower()}2{tospace.lower()}",
+                        module_cpu=skimage.color,
+                        module_gpu=cucim.skimage.color,
+                        run_cpu=run_cpu,
+                    )
+                    results = B.run_benchmark(duration=1)
+                    all_results = pd.concat([all_results, results["full"]])
+
+        elif function_name == 'rgba2rgb':
+            B = RGBABench(
+                function_name="rgba2rgb",
+                shape=shape[:-1] + (4,),
                 dtypes=dtypes,
-                fixed_kwargs=dict(fromspace=fromspace, tospace=tospace),
+                fixed_kwargs={},
                 var_kwargs={},
-                index_str=f"{fromspace.lower()}2{tospace.lower()}",
                 module_cpu=skimage.color,
                 module_gpu=cucim.skimage.color,
+                run_cpu=run_cpu,
             )
             results = B.run_benchmark(duration=1)
             all_results = pd.concat([all_results, results["full"]])
 
-    # rgb2hed and hed2rgb test combine_stains and separate_stains and all other
-    # stains should have equivalent performance.
-    #
-    # Probably not necessary to benchmark combine_stains and separate_stains
-    # e.g.
-    #    ihc_hdx = separate_stains(ihc, hdx_from_rgb)
-    #    ihc = combine_stains(ihc_hdx, rgb_from_hdx)
-    #
+        elif function_name == 'label2rgb':
 
-    for fname in ["rgb2hed", "hed2rgb", "lab2lch", "lch2lab", "xyz2lab",
-                  "lab2xyz"]:
-        B = ColorBench(
-            function_name=fname,
-            shape=shape,
-            dtypes=dtypes,
-            fixed_kwargs={},
-            var_kwargs={},
-            module_cpu=skimage.color,
-            module_gpu=cucim.skimage.color,
-        )
-        results = B.run_benchmark(duration=1)
-        all_results = pd.concat([all_results, results["full"]])
+            for contiguous_labels in [True, False]:
+                if contiguous_labels:
+                    index_str = "contiguous"
+                else:
+                    index_str = None
+                B = LabelBench(
+                    function_name="label2rgb",
+                    shape=shape,
+                    dtypes=dtypes,
+                    contiguous_labels=contiguous_labels,
+                    index_str=index_str,
+                    fixed_kwargs=dict(bg_label=0),
+                    var_kwargs=dict(kind=["avg", "overlay"]),
+                    module_cpu=skimage.color,
+                    module_gpu=cucim.skimage.color,
+                    run_cpu=run_cpu,
+                )
+                results = B.run_benchmark(duration=1)
+                all_results = pd.concat([all_results, results["full"]])
 
-    B = RGBABench(
-        function_name="rgba2rgb",
-        shape=shape[:-1] + (4,),
-        dtypes=dtypes,
-        fixed_kwargs={},
-        var_kwargs={},
-        module_cpu=skimage.color,
-        module_gpu=cucim.skimage.color,
-    )
-    results = B.run_benchmark(duration=1)
-    all_results = pd.concat([all_results, results["full"]])
+        elif function_name in [
+            'rgb2hed', 'hed2rgb', 'lab2lch', 'lch2lab', 'xyz2lab', 'lab2xyz'
+        ]:
+            B = ColorBench(
+                function_name=function_name,
+                shape=shape + (3,),
+                dtypes=dtypes,
+                fixed_kwargs={},
+                var_kwargs={},
+                module_cpu=skimage.color,
+                module_gpu=cucim.skimage.color,
+                run_cpu=run_cpu,
+            )
+            results = B.run_benchmark(duration=1)
+            all_results = pd.concat([all_results, results["full"]])
 
-    for contiguous_labels in [True, False]:
-        if contiguous_labels:
-            index_str = "contiguous"
-        else:
-            index_str = None
-        B = LabelBench(
-            function_name="label2rgb",
-            shape=shape,
-            dtypes=dtypes,
-            contiguous_labels=contiguous_labels,
-            index_str=index_str,
-            fixed_kwargs=dict(bg_label=0),
-            var_kwargs=dict(kind=["avg", "overlay"]),
-            module_cpu=skimage.color,
-            module_gpu=cucim.skimage.color,
-        )
-        results = B.run_benchmark(duration=1)
-        all_results = pd.concat([all_results, results["full"]])
 
-fbase = os.path.splitext(pfile)[0]
-all_results.to_csv(fbase + ".csv")
-all_results.to_pickle(pfile)
-with open(fbase + ".md", "wt") as f:
-    f.write(all_results.to_markdown())
+    fbase = os.path.splitext(pfile)[0]
+    all_results.to_csv(fbase + ".csv")
+    all_results.to_pickle(pfile)
+    try:
+        import tabular
+
+        with open(fbase + ".md", "wt") as f:
+            f.write(all_results.to_markdown())
+    except ImportError:
+        pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Benchmarking cuCIM color conversion functions')
+    func_name_choices = ['convert_colorspace', 'rgb2hed', 'hed2rgb', 'lab2lch', 'lch2lab', 'xyz2lab', 'lab2xyz', 'rgba2rgb', 'label2rgb']
+    dtype_choices = ['float16', 'float32', 'float64', 'int8', 'int16', 'int32', 'int64', 'uint8', 'uint16', 'uint32', 'uint64']
+    parser.add_argument('-i','--img_size', type=str, help='Size of input image (omit color channel, it will be appended as needed)', required=True)
+    parser.add_argument('-d','--dtype', type=str, help='Dtype of input image', choices = dtype_choices, required=True)
+    parser.add_argument('-f','--func_name', type=str, help='function to benchmark', choices = func_name_choices, required=True)
+    parser.add_argument('-t','--duration', type=int, help='time to run benchmark', required=True)
+    parser.add_argument('--no_cpu', action='store_true', help='disable cpu measurements', default=False)
+
+    args = parser.parse_args()
+    main(args)
