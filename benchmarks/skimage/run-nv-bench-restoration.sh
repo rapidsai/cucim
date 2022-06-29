de8f2928cb3554e4207ed2df1ab1a347856faef7
Gregory Lee <grlee77@gmail.com>
2022-06-29 15:15:25 -0400

    update restoration benchmarks

---
+++ benchmarks/skimage/run-nv-bench-restoration.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+param_shape=(512,512)  #  3840,2160 64,64,64 256,256,256)
+param_filt=(denoise_tv_chambolle calibrate_denoiser wiener unsupervised_wiener richardson_lucy)
+param_dt=(float32 uint8)
+for shape in "${param_shape[@]}"; do
+    for filt in "${param_filt[@]}"; do
+        for dt in "${param_dt[@]}"; do
+            python cucim_restoration_bench.py -f $filt -i $shape -d $dt -t 10
+        done
+    done
+done
