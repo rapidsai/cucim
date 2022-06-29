afeab693d778302419c1808f64a9f0003eca9df9
Gregory Lee <grlee77@gmail.com>
2022-06-29 10:57:58 -0400

    expand dtype choices and use names recognized by NumPy

--- benchmarks/skimage/run-nv-bench-filters.sh
+++ benchmarks/skimage/run-nv-bench-filters.sh
@@ -2,7 +2,7 @@
 param_shape=(512,512 3840,2160 3840,2160,3 192,192,192)
 param_filt=(gabor gaussian median rank_order unsharp_mask sobel prewitt scharr roberts roberts_pos_diag roberts_neg_diag farid laplace meijering sato frangi hessian threshold_isodata threshold_otsu threshold_yen threshold_local threshold_li threshold_minimum threshold_mean threshold_triangle threshold_niblack threshold_sauvola apply_hysteresis_threshold threshold_multiotsu)
 # param_filt=(rank_order )
-param_dt=('fp64' 'fp32' 'fp16')
+param_dt=('float64' 'float32' 'float16')
 for shape in "${param_shape[@]}"; do
     for filt in "${param_filt[@]}"; do
         for dt in "${param_dt[@]}"; do
