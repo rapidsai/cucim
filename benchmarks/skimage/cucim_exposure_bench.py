78d1dcc0fdf93c7480d3a5aadb71c82b52181c7a
Gregory Lee <grlee77@gmail.com>
2022-06-29 11:15:27 -0400

    update exposure benchmarks

--- benchmarks/skimage/cucim_exposure_bench.py
+++ benchmarks/skimage/cucim_exposure_bench.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import pickle
 
@@ -39,95 +40,126 @@ class MatchHistogramBench(ImageBench):
         self.args_gpu = (imaged, imaged2)
 
 
-pfile = "cucim_exposure_results.pickle"
-if os.path.exists(pfile):
-    with open(pfile, "rb") as f:
-        all_results = pickle.load(f)
-else:
-    all_results = pd.DataFrame()
-dtypes = [np.uint8, np.float32]
+def main(args):
 
-exposure_config = {
-    "equalize_adapthist": dict(
-        fixed_kwargs=dict(clip_limit=0.01, nbins=256),
-        variable_kwargs=dict(),
-        color_required=False,
-        grayscale_only=False,
-        dtypes=None,
-        shapes=None,
-    ),
-    "histogram": dict(
-        fixed_kwargs=dict(source_range="image"),
-        variable_kwargs=dict(nbins=[16, 256], normalize=[True, False]),
-        color_required=False,
-        grayscale_only=True,
-        dtypes=None,
-        shapes=None,
-    ),
-}
+    pfile = "cucim_exposure_results.pickle"
+    if os.path.exists(pfile):
+        with open(pfile, "rb") as f:
+            all_results = pickle.load(f)
+    else:
+        all_results = pd.DataFrame()
 
-for function_name, fixed_kwargs, var_kwargs, allow_color in [
-    ("equalize_adapthist", dict(clip_limit=0.01, nbins=256), dict(), True),
-    (
-        "histogram",
-        dict(source_range="image"),
-        dict(nbins=[16, 256], normalize=[True, False]),
-        False,
-    ),
-    ("cumulative_distribution", dict(), dict(nbins=[16, 256]), False),
-    ("equalize_hist", dict(mask=None), dict(nbins=[16, 256]), False),
-    ("rescale_intensity", dict(in_range="image", out_range="dtype"), dict(), False),
-    ("adjust_gamma", dict(), dict(), False),
-    ("adjust_log", dict(), dict(), False),
-    ("adjust_sigmoid", dict(), dict(inv=[False, True]), False),
-    ("is_low_contrast", dict(), dict(), False),
-]:
+    dtypes = [np.dtype(args.dtype)]
+    # image sizes/shapes
+    shape = tuple(list(map(int,(args.img_size.split(',')))))
+    run_cpu = not args.no_cpu
 
-    for shape in [(512, 512), (3840, 2160), (3840, 2160, 3), (192, 192, 192)]:
-        ndim = len(shape)
-        if shape[-1] == 3 and not allow_color:
+    exposure_config = {
+        "equalize_adapthist": dict(
+            fixed_kwargs=dict(clip_limit=0.01, nbins=256),
+            variable_kwargs=dict(),
+            color_required=False,
+            grayscale_only=False,
+            dtypes=None,
+            shapes=None,
+        ),
+        "histogram": dict(
+            fixed_kwargs=dict(source_range="image"),
+            variable_kwargs=dict(nbins=[16, 256], normalize=[True, False]),
+            color_required=False,
+            grayscale_only=True,
+            dtypes=None,
+            shapes=None,
+        ),
+    }
+
+
+    for function_name, fixed_kwargs, var_kwargs, allow_color in [
+        ("equalize_adapthist", dict(clip_limit=0.01, nbins=256), dict(), True),
+        (
+            "histogram",
+            dict(source_range="image"),
+            dict(nbins=[16, 256], normalize=[True, False]),
+            False,
+        ),
+        ("cumulative_distribution", dict(), dict(nbins=[16, 256]), False),
+        ("equalize_hist", dict(mask=None), dict(nbins=[16, 256]), False),
+        ("rescale_intensity", dict(in_range="image", out_range="dtype"), dict(), False),
+        ("adjust_gamma", dict(), dict(), False),
+        ("adjust_log", dict(), dict(), False),
+        ("adjust_sigmoid", dict(), dict(inv=[False, True]), False),
+        ("is_low_contrast", dict(), dict(), False),
+    ]:
+
+        if function_name != args.func_name:
             continue
 
-        if function_name == "equalize_adapthist":
-            #  TODO: fix equalize_adapthist for size (3840, 2160) and kernel_size = [16, 16]
-            size_factors = [4, 8, 16]
-            kernel_sizes = []
-            for size_factor in size_factors:
-                kernel_sizes.append([max(s // size_factor, 1) for s in shape if s != 3])
-            var_kwargs.update(dict(kernel_size=kernel_sizes))
+        ndim = len(shape)
 
-        B = ExposureBench(
-            function_name=function_name,
-            shape=shape,
-            dtypes=dtypes,
-            fixed_kwargs=fixed_kwargs,
-            var_kwargs=var_kwargs,
-            module_cpu=skimage.exposure,
-            module_gpu=cucim.skimage.exposure,
-        )
-        results = B.run_benchmark(duration=1)
-        all_results = pd.concat([all_results, results["full"]])
+        if function_name == 'match_histograms':
+
+            channel_axis = -1 if shape[-1] in [3, 4] else None
+
+            B = MatchHistogramBench(
+                function_name="match_histograms",
+                shape=shape,
+                dtypes=dtypes,
+                fixed_kwargs=dict(channel_axis=channel_axis),
+                var_kwargs=dict(),
+                module_cpu=skimage.exposure,
+                module_gpu=cucim.skimage.exposure,
+                run_cpu=run_cpu,
+            )
+            results = B.run_benchmark(duration=1)
+            all_results = pd.concat([all_results, results["full"]])
+
+        else:
+
+            if shape[-1] == 3 and not allow_color:
+                continue
+
+            if function_name == "equalize_adapthist":
+                #  TODO: fix equalize_adapthist for size (3840, 2160) and kernel_size = [16, 16]
+                size_factors = [4, 8, 16]
+                kernel_sizes = []
+                for size_factor in size_factors:
+                    kernel_sizes.append([max(s // size_factor, 1) for s in shape if s != 3])
+                var_kwargs.update(dict(kernel_size=kernel_sizes))
+
+            B = ExposureBench(
+                function_name=function_name,
+                shape=shape,
+                dtypes=dtypes,
+                fixed_kwargs=fixed_kwargs,
+                var_kwargs=var_kwargs,
+                module_cpu=skimage.exposure,
+                module_gpu=cucim.skimage.exposure,
+                run_cpu=run_cpu,
+            )
+            results = B.run_benchmark(duration=1)
+            all_results = pd.concat([all_results, results["full"]])
+
+    fbase = os.path.splitext(pfile)[0]
+    all_results.to_csv(fbase + ".csv")
+    all_results.to_pickle(pfile)
+    try:
+        import tabulate
+
+        with open(fbase + ".md", "wt") as f:
+            f.write(all_results.to_markdown())
+    except ImportError:
+        pass
 
 
-for shape in [(512, 512), (3840, 2160), (3840, 2160, 3), (192, 192, 192)]:
-    ndim = len(shape)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Benchmarking cuCIM exposure functions')
+    func_name_choices = ['equalize_adapthist', 'cumulative_distribution', 'equalize_hist', 'rescale_intensity', 'adjust_gamma', 'adjust_log', 'adjust_sigmoid', 'is_low_contrast', 'match_histograms']
+    dtype_choices = ['float16', 'float32', 'float64', 'int8', 'int16', 'int32', 'int64', 'uint8', 'uint16', 'uint32', 'uint64']
+    parser.add_argument('-i','--img_size', type=str, help='Size of input image', required=True)
+    parser.add_argument('-d','--dtype', type=str, help='Dtype of input image', choices=dtype_choices, required=True)
+    parser.add_argument('-f','--func_name', type=str, help='function to benchmark', choices=func_name_choices, required=True)
+    parser.add_argument('-t','--duration', type=int, help='time to run benchmark', required=True)
+    parser.add_argument('--no_cpu', action='store_true', help='disable cpu measurements', default=False)
 
-    channel_axis = -1 if shape[-1] in [3, 4] else None
-
-    B = MatchHistogramBench(
-        function_name="match_histograms",
-        shape=shape,
-        dtypes=dtypes,
-        fixed_kwargs=dict(channel_axis=channel_axis),
-        var_kwargs=dict(),
-        module_cpu=skimage.exposure,
-        module_gpu=cucim.skimage.exposure,
-    )
-    results = B.run_benchmark(duration=1)
-    all_results = pd.concat([all_results, results["full"]])
-
-fbase = os.path.splitext(pfile)[0]
-all_results.to_csv(fbase + ".csv")
-all_results.to_pickle(pfile)
-with open(fbase + ".md", "wt") as f:
-    f.write(all_results.to_markdown())
+    args = parser.parse_args()
+    main(args)
