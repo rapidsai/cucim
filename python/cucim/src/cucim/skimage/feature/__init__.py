0b222d45dd24f37dbbcd9aeb6818a48eccd3201a
Gregory Lee <grlee77@gmail.com>
2022-07-26 08:06:14 -0400

    add match_descriptors

--- python/cucim/src/cucim/skimage/feature/__init__.py
+++ python/cucim/src/cucim/skimage/feature/__init__.py
@@ -6,38 +6,9 @@ from .corner import (corner_foerstner, corner_harris, corner_kitchen_rosenfeld,
                      corner_peaks, corner_shi_tomasi, hessian_matrix,
                      hessian_matrix_det, hessian_matrix_eigvals, shape_index,
                      structure_tensor, structure_tensor_eigenvalues)
+from .match import match_descriptors
 from .peak import peak_local_max
 from .template import match_template
-
-
-@deprecated(
-    alt_func="cucim.skimage.registration.phase_cross_correlation",
-    removed_version="0.19",
-)
-def masked_register_translation(
-    src_image, target_image, src_mask, target_mask=None, overlap_ratio=0.3
-):
-    from ..registration import phase_cross_correlation
-
-    return phase_cross_correlation(
-        src_image,
-        target_image,
-        reference_mask=src_mask,
-        moving_mask=target_mask,
-        overlap_ratio=overlap_ratio,
-    )
-
-
-@deprecated(
-    alt_func="cucim.skimage.registration.phase_cross_correlation",
-    removed_version="0.19",
-)
-def register_translation(
-    src_image, target_image, upsample_factor=1, space="real", return_error=True
-):
-    from ..registration._phase_cross_correlation import \
-        phase_cross_correlation as func
-    return func(src_image, target_image, upsample_factor, space, return_error)
 
 
 __all__ = ['canny',
@@ -61,5 +32,4 @@ __all__ = ['canny',
            # 'corner_fast',
            # 'corner_orientations',
            'match_template',
-           'register_translation',
-           'masked_register_translation']
+           'match_descriptors']
