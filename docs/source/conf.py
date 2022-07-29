c78f163006d550179f7e76e9c7e697c9c777f691
galipremsagar <sagarprem75@gmail.com>
2022-07-29 08:25:06 -0700

    theme fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -182,5 +182,6 @@ numpydoc_class_members_toctree = False
 
 
 def setup(app):
-    app.add_css_file('params.css')
+    app.add_css_file('custom.css')
+    app.add_js_file('custom.js')
     app.add_css_file("https://docs.rapids.ai/assets/css/custom.css")
