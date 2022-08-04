fef4f65c16b110ee01c72ad050149f26aeeb1a90
galipremsagar <sagarprem75@gmail.com>
2022-08-04 09:00:44 -0700

    Defer loading of custom.js

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -190,4 +190,4 @@ numpydoc_class_members_toctree = False
 
 def setup(app):
     app.add_css_file("https://docs.rapids.ai/assets/css/custom.css")
-    app.add_js_file("https://docs.rapids.ai/assets/js/custom.js")
+    app.add_js_file("https://docs.rapids.ai/assets/js/custom.js", loading_method="defer")
