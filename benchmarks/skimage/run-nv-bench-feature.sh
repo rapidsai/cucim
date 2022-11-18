8b92401a8276fe83a5ebac7e174af073a58b5fc3
Rune <60315601+monzelr@users.noreply.github.com>
2022-11-18 14:53:43 +0100

    Add blob feature detectors (blob_dog, blob_log, blob_doh) (#413)
    Hi again,
    due to renaming my branch to `add-blob-detection`, the old PR #411 had been closed.
    
    This PR solves issue #398.
    
    Basically, I followed the suggestions by @grlee77 and here is the difference to PR #411:
    - replaced the draw.disk and draw.ellipsoid GPU functions by the CPU functions (skimage.draw.disk / ellipsoid) in the unit tests, the matrices moved to GPU via `cupy.asarray`
    - removed the submodule cucim.skimage.draw becaused it is not needed anymore
    - deleted two unit tests because `_blob_overlap` is implemented in c++/cuda and not in python anymore
    
    Again thanks to @annendominik who helped with the implementation.
    
    And also thank you for your help @grlee77  - it is the first time I do a PR in github. ;-)
    
    Authors:
      - Rune (https://github.com/monzelr)
      - Gregory Lee (https://github.com/grlee77)
    
    Approvers:
      - Gregory Lee (https://github.com/grlee77)
    
    URL: https://github.com/rapidsai/cucim/pull/413

--- benchmarks/skimage/run-nv-bench-feature.sh
+++ benchmarks/skimage/run-nv-bench-feature.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 param_shape=(512,512 3840,2160 3840,2160,3 192,192,192)
-param_filt=(multiscale_basic_features canny daisy structure_tensor hessian_matrix hessian_matrix_det shape_index corner_kitchen_rosenfeld corner_harris corner_shi_tomasi corner_foerstner corner_peaks match_template)
-param_dt=(float64 float32 float16)
+param_filt=(multiscale_basic_features canny daisy structure_tensor hessian_matrix hessian_matrix_det shape_index corner_kitchen_rosenfeld corner_harris corner_shi_tomasi corner_foerstner corner_peaks match_template blob_dog blob_log blob_doh)
+param_dt=(float64 float32)
 for shape in "${param_shape[@]}"; do
     for filt in "${param_filt[@]}"; do
         for dt in "${param_dt[@]}"; do
-            python cucim_feature_bench.py -f $filt -i $shape -d $dt -t 10
+            python cucim_feature_bench.py -f $filt -i $shape -d $dt -t 3
         done
     done
 done
