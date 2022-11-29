f5408d0183e6013318875aaee09fbe9fcd401977
Gregory Lee <grlee77@gmail.com>
2022-11-29 09:45:24 -0500

    add isotropic binary morphology functions (#421)
    related to #419
    
    These are based on the distance transform and are a faster way of computing binary morphological operations for large diameter disk or ball footprints. Unlike the sequence footprint decomposition methods, the footprint is exactly circular (spherical).
    
    One test case `test_isotropic_erosion_spacing` will require #407 to be merged first. 
    
    #406 will also improve the performance of this implementation
    
    Authors:
      - Gregory Lee (https://github.com/grlee77)
    
    Approvers:
      - Gigon Bae (https://github.com/gigony)
    
    URL: https://github.com/rapidsai/cucim/pull/421

--- benchmarks/skimage/run-nv-bench-morphology.sh
+++ benchmarks/skimage/run-nv-bench-morphology.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 param_shape=(512,512 3840,2160 3840,2160,3 192,192,192)
-param_filt=(binary_erosion binary_dilation binary_opening binary_closing remove_small_objects remove_small_holes erosion dilation opening closing white_tophat black_tophat medial_axis thin reconstruction)
-
+param_filt=(binary_erosion binary_dilation binary_opening binary_closing isotropic_erosion isotropic_dilation isotropic_opening isotropic_closing remove_small_objects remove_small_holes erosion dilation opening closing white_tophat black_tophat medial_axis thin reconstruction)
 param_dt=(uint8)
 for shape in "${param_shape[@]}"; do
     for filt in "${param_filt[@]}"; do
         for dt in "${param_dt[@]}"; do
-            python cucim_morphology_bench.py -f $filt -i $shape -d $dt -t 10
+            python cucim_morphology_bench.py -f $filt -i $shape -d $dt -t 4
         done
     done
 done
@@ -18,7 +17,7 @@ param_dt=(float32)
 for shape in "${param_shape[@]}"; do
     for filt in "${param_filt[@]}"; do
         for dt in "${param_dt[@]}"; do
-            python cucim_morphology_bench.py -f $filt -i $shape -d $dt -t 10
+            python cucim_morphology_bench.py -f $filt -i $shape -d $dt -t 4
         done
     done
 done
