0193e8bce054bdd23e6d4e8fd9e6226c17d44831
AJ Schmidt <aschmidt@nvidia.com>
2023-01-01 13:49:36 -0500

    fix/reorder `mamba install` pkgs

--- ci/test_notebooks.sh
+++ ci/test_notebooks.sh
@@ -27,7 +27,7 @@ PYTHON_CHANNEL=$(rapids-download-conda-from-s3 python)
 rapids-mamba-retry install \
   --channel "${CPP_CHANNEL}" \
   --channel "${PYTHON_CHANNEL}" \
-  cudf libcudf
+  libcucim cucim
 
 NBTEST="$(realpath "$(dirname "$0")/utils/nbtest.sh")"
 pushd notebooks
