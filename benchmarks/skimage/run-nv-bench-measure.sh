4b19225a599035b72a0888104a0ef675e9f9f1be
Gregory Lee <grlee77@gmail.com>
2023-02-01 23:48:23 -0800

    Add colocalization measures (#488)
    This MR impelements four colocaliziation measures being introduced for scikit-image 0.20. The implementation of each is fairly simple. For reference, see a related [gallery example](https://scikit-image.org/docs/dev/auto_examples/applications/plot_colocalization_metrics.html#sphx-glr-auto-examples-applications-plot-colocalization-metrics-py).
    
    ## Benchmark results vs. scikit-image 0.20dev
    
    Benchmark results show speedup in all cases below aside from `intersection_coeff` on small images.
    
    function              | shape         | dtype   | acceleration
    ----------------------|---------------|---------|-------------
    manders_coloc_coeff   | (512, 512)    | float32 |  1.72
    manders_coloc_coeff   | (512, 512)    | uint8   |  2.93
    manders_coloc_coeff   | (3840, 2160)  | float32 | 39.3
    manders_coloc_coeff   | (3840, 2160)  | uint8   | 49.9
    manders_coloc_coeff   | (192,192,192) | float32 | 38.1
    manders_coloc_coeff   | (192,192,192) | uint8   | 47.4
    
    function              | shape         | dtype   | acceleration
    ----------------------|---------------|---------|-------------
    manders_overlap_coeff | (512, 512)    | float32 |  1.41
    manders_overlap_coeff | (512, 512)    | uint8   |  1.48
    manders_overlap_coeff | (3840, 2160)  | float32 |  2.33
    manders_overlap_coeff | (3840, 2160)  | uint8   |  2.93
    manders_overlap_coeff | (192,192,192) | float32 |  2.31
    manders_overlap_coeff | (192,192,192) | uint8   |  2.80
    
    function              | shape         | dtype   | acceleration
    ----------------------|---------------|---------|-------------
    pearson_corr_coeff    | (512, 512)    | float32 |  6.96
    pearson_corr_coeff    | (512, 512)    | uint8   |  8.10
    pearson_corr_coeff    | (3840, 2160)  | float32 | 49.5
    pearson_corr_coeff    | (3840, 2160)  | uint8   | 53.4
    pearson_corr_coeff    | (192,192,192) | float32 | 51.6
    pearson_corr_coeff    | (192,192,192) | uint8   | 52.5
    
    function              | shape         | dtype   | acceleration
    ----------------------|---------------|---------|-------------
    intersection_coeff    | (512, 512)    | bool    |  0.278
    intersection_coeff    | (3840, 2160)  | bool    | 13.1
    intersection_coeff    | (192,192,192) | bool    | 11.1
    
    Authors:
      - Gregory Lee (https://github.com/grlee77)
    
    Approvers:
      - Gigon Bae (https://github.com/gigony)
    
    URL: https://github.com/rapidsai/cucim/pull/488

--- benchmarks/skimage/run-nv-bench-measure.sh
+++ benchmarks/skimage/run-nv-bench-measure.sh
@@ -9,3 +9,28 @@ for shape in "${param_shape[@]}"; do
         done
     done
 done
+
+# # commenting out colocalization metrics below this point
+# # (scikit-image 0.20 is not yet officially released)
+# param_shape=(512,512 3840,2160 192,192,192)
+# param_filt=(manders_coloc_coeff manders_overlap_coeff pearson_corr_coeff)
+# param_dt=(float32 uint8)
+# for shape in "${param_shape[@]}"; do
+#     for filt in "${param_filt[@]}"; do
+#         for dt in "${param_dt[@]}"; do
+#             python cucim_measure_bench.py -f $filt -i $shape -d $dt -t 10
+#         done
+#     done
+# done
+
+# # only supports binary-valued images
+# param_shape=(512,512 3840,2160 192,192,192)
+# param_filt=(intersection_coeff)
+# param_dt=(bool)
+# for shape in "${param_shape[@]}"; do
+#     for filt in "${param_filt[@]}"; do
+#         for dt in "${param_dt[@]}"; do
+#             python cucim_measure_bench.py -f $filt -i $shape -d $dt -t 10
+#         done
+#     done
+# done
