9c4d5ce2545689a67babf5faf0ffd64cc408c969
Gregory Lee <grlee77@gmail.com>
2023-10-28 15:36:47 -0400

    skip single test case requiring libopenslide

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -6,20 +6,6 @@ set -eou pipefail
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="cucim_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 
-# some test cases require libopenslide.so for writing the test TIFF images.
-
-# Building the libjpeg-turbo dependency requires YASM
-# Also need to install openslide dev libraries on the system
-if ! command -v apt &> /dev/null
-then
-    echo "apt package manager not found, attempting to use yum"
-    yum install openslide-devel -y
-else
-    echo "apt package manager was found"
-    apt update
-    apt install libopenslide-dev -y
-fi
-
 # echo to expand wildcard before adding `[extra]` requires for pip
 python -m pip install $(echo ./dist/cucim*.whl)[test]
 
@@ -27,7 +13,7 @@ python -m pip install $(echo ./dist/cucim*.whl)[test]
 if [ "$(arch)" == "aarch64" && ${RAPIDS_BUILD_TYPE} == "pull-request" ]; then
     python ./ci/wheel_smoke_test.py
 else
-    # Note: imagecodecs is currently not installed on aarch64, so a handful of
-    #       test cases will be skipped in that case
-    python -m pytest ./python/cucim
+    # skip `test_converter`` (which requires openslide-python and libopenslide)
+    # (a segfault was observed when using the system libopenslide-dev)
+    python -m pytest ./python/cucim -k "not test_converter"
 fi
