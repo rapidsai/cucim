fe2ebd9deafab99e1f48900913fbd65eb766386b
Gregory Lee <grlee77@gmail.com>
2023-11-01 19:48:11 -0400

    apply black globally

--- ci/wheel_smoke_test.py
+++ ci/wheel_smoke_test.py
@@ -6,14 +6,14 @@ import cucim.skimage
 
 if __name__ == "__main__":
     # verify that all top-level modules are available
-    assert cucim.is_available('clara')
-    assert cucim.is_available('core')
-    assert cucim.is_available('skimage')
+    assert cucim.is_available("clara")
+    assert cucim.is_available("core")
+    assert cucim.is_available("skimage")
 
     # generate a synthetic image and apply a filter
     img = cucim.skimage.data.binary_blobs(length=512, n_dim=2)
     assert isinstance(img, cp.ndarray)
-    assert img.dtype.kind == 'b'
+    assert img.dtype.kind == "b"
     assert img.shape == (512, 512)
 
     eroded = cucim.skimage.morphology.binary_erosion(
