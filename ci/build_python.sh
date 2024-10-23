c7ae323b0be222678e2a27f0951b764d30659fb7
James Lamb <jlamb@nvidia.com>
2024-10-23 11:14:50 -0500

    print sccache stats, reduce wheel build verbosity

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -20,11 +20,15 @@ conda config --set path_conflict prevent
 
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 
+sccache --zero-stats
+
 # TODO: Remove `--no-test` flag once importing on a CPU
 # node works correctly
 RAPIDS_PACKAGE_VERSION=$(head -1 ./VERSION) rapids-conda-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
   conda/recipes/cucim
+
+sccache --show-adv-stats
 
 rapids-upload-conda-to-s3 python
--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 
 package_name="cucim"
 package_dir="python/cucim"
-package_src_dir="${package_dir}/src/${package_name}"
 
 CMAKE_BUILD_TYPE="release"
 
@@ -18,26 +17,15 @@ RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 
 PACKAGE_CUDA_SUFFIX="-${RAPIDS_PY_CUDA_SUFFIX}"
 
-rapids-logger "Generating build requirements"
-matrix_selectors="cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION};cuda_suffixed=true"
-
+# Install pip build dependencies (not yet using pyproject.toml)
 rapids-dependency-file-generator \
-  --output requirements \
-  --file-key "py_build_${package_name}" \
-  --matrix "${matrix_selectors}" \
-| tee /tmp/requirements-build.txt
+  --file-key "py_build" \
+  --output "requirements" \
+  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" | tee build_requirements.txt
 
-rapids-dependency-file-generator \
-  --output requirements \
-  --file-key "py_rapids_build_${package_name}" \
-  --matrix "${matrix_selectors}" \
-| tee -a /tmp/requirements-build.txt
+python -m pip install -r build_requirements.txt
 
-rapids-logger "Installing build requirements"
-python -m pip install \
-    -v \
-    --prefer-binary \
-    -r /tmp/requirements-build.txt
+sccache --zero-stats
 
 # First build the C++ lib using CMake via the run script
 ./run build_local all ${CMAKE_BUILD_TYPE}
@@ -46,11 +34,12 @@ sccache --show-adv-stats
 
 cd "${package_dir}"
 
+sccache --zero-stats
+
 rapids-logger "Building '${package_name}' wheel"
 python -m pip wheel \
     -w dist \
     -v \
-    --no-build-isolation \
     --no-deps \
     --disable-pip-version-check \
     .
