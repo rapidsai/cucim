90d0e90392cda08e616303a3cfa4a3823474ba1f
James Lamb <jlamb@nvidia.com>
2024-11-13 11:36:05 -0600

    enforce wheel size limits, README formatting in CI

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -56,4 +56,6 @@ mkdir -p final_dist
 python -m auditwheel repair -w final_dist dist/*
 ls -1 final_dist | grep -vqz 'none'
 
+../../ci/validate_wheel.sh final_dist
+
 RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 final_dist
