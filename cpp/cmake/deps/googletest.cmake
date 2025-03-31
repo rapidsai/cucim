40e5d0fc242be32285db50eb9a4139467664fe17
Gigon Bae <gbae@nvidia.com>
2025-03-31 15:37:48 -0700

    Update googletest version to v1.16.0
    Signed-off-by: Gigon Bae <gbae@nvidia.com>

--- cpp/cmake/deps/googletest.cmake
+++ cpp/cmake/deps/googletest.cmake
@@ -17,7 +17,7 @@ if (NOT TARGET deps::googletest)
     FetchContent_Declare(
             deps-googletest
             GIT_REPOSITORY https://github.com/google/googletest.git
-            GIT_TAG release-1.10.0
+            GIT_TAG v1.16.0
             GIT_SHALLOW TRUE
             EXCLUDE_FROM_ALL
     )
