0941a54df41c024084dcb4fcdf04f229a6bb86d1
cdinea <123968711+cdinea@users.noreply.github.com>
2025-11-21 14:20:51 -0800

    Update cpp/plugins/cucim.kit.cuslide2/cmake/modules/CuCIMUtils.cmake
    Co-authored-by: Kyle Edwards <kyedwards@nvidia.com>

--- cpp/plugins/cucim.kit.cuslide2/cmake/modules/CuCIMUtils.cmake
+++ cpp/plugins/cucim.kit.cuslide2/cmake/modules/CuCIMUtils.cmake
@@ -4,28 +4,3 @@
 # SPDX-License-Identifier: Apache-2.0
 # cmake-format: on
 #
-
-# Store current BUILD_SHARED_LIBS setting in CUCIM_OLD_BUILD_SHARED_LIBS
-if(NOT COMMAND cucim_set_build_shared_libs)
-    macro(cucim_set_build_shared_libs new_value)
-        set(CUCIM_OLD_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS}})
-        if (DEFINED CACHE{BUILD_SHARED_LIBS})
-            set(CUCIM_OLD_BUILD_SHARED_LIBS_CACHED TRUE)
-        else()
-            set(CUCIM_OLD_BUILD_SHARED_LIBS_CACHED FALSE)
-        endif()
-        set(BUILD_SHARED_LIBS ${new_value} CACHE BOOL "" FORCE)
-    endmacro()
-endif()
-
-# Restore BUILD_SHARED_LIBS setting from CUCIM_OLD_BUILD_SHARED_LIBS
-if(NOT COMMAND cucim_restore_build_shared_libs)
-    macro(cucim_restore_build_shared_libs)
-        if (CUCIM_OLD_BUILD_SHARED_LIBS_CACHED)
-            set(BUILD_SHARED_LIBS ${CUCIM_OLD_BUILD_SHARED_LIBS} CACHE BOOL "" FORCE)
-        else()
-            unset(BUILD_SHARED_LIBS CACHE)
-            set(BUILD_SHARED_LIBS ${CUCIM_OLD_BUILD_SHARED_LIBS})
-        endif()
-    endmacro()
-endif()
