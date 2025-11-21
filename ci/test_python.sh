26afd46cb3aa402c159c8a3bbc8e301b4b3bac51
Bradley Dice <bdice@bradleydice.com>
2025-11-21 10:50:39 -0600

    Use strict priority in CI conda tests (#979)
    This PR sets conda to use `strict` priority in CI tests.
    
    Mixing channel priority is frequently a cause of unexpected errors. Our CI jobs should always use strict priority in order to enforce that conda packages come from local channels with the artifacts built in CI, not mixing with older nightly artifacts from the `rapidsai-nightly` channel or other sources.
    
    xref: https://github.com/rapidsai/build-planning/issues/14
    
    Authors:
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - Gil Forsyth (https://github.com/gforsyth)
    
    URL: https://github.com/rapidsai/cucim/pull/979

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2022-2023, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2022-2025, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
 # Common setup steps shared by Python test jobs
@@ -7,6 +7,9 @@
 set -euo pipefail
 
 . /opt/conda/etc/profile.d/conda.sh
+
+rapids-logger "Configuring conda strict channel priority"
+conda config --set channel_priority strict
 
 rapids-logger "Downloading artifacts from previous jobs"
 CPP_CHANNEL=$(rapids-download-conda-from-github cpp)
