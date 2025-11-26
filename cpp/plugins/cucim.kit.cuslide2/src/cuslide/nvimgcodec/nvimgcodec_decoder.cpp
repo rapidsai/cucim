20343a5e80a647e0ca14fd2e4c31bfcf29cc0236
cdinea <cdinea@nvidia.com>
2025-11-25 21:29:11 -0800

    Update cuslide2 plugin for nvImageCodec v0.7.0

--- cpp/plugins/cucim.kit.cuslide2/src/cuslide/nvimgcodec/nvimgcodec_decoder.cpp
+++ cpp/plugins/cucim.kit.cuslide2/src/cuslide/nvimgcodec/nvimgcodec_decoder.cpp
@@ -283,7 +283,7 @@ bool decode_ifd_region_nvimgcodec(const IfdInfo& ifd_info,
         output_image_info.plane_info[0].num_channels = num_channels;
         output_image_info.plane_info[0].row_stride = row_stride;
         output_image_info.plane_info[0].sample_type = NVIMGCODEC_SAMPLE_DATA_TYPE_UINT8;
-        output_image_info.buffer_size = buffer_size;
+        // Note: buffer_size removed in nvImageCodec v0.7.0 - size is inferred from plane_info
         output_image_info.cuda_stream = 0;
 
         #ifdef DEBUG
