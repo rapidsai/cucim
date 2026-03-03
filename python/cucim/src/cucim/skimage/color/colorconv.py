8d15e8da5ffea663a33b6bf6e8fc28f9f5e52564
Gregory R. Lee <grelee@nvidia.com>
2026-03-03 15:07:48 -0500

    docstring fixes in skimage.color module

--- python/cucim/src/cucim/skimage/color/colorconv.py
+++ python/cucim/src/cucim/skimage/color/colorconv.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2009-2022 the scikit-image team
-# SPDX-FileCopyrightText: Copyright (c) 2021-2025, NVIDIA CORPORATION. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2021-2026, NVIDIA CORPORATION. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0 AND BSD-3-Clause
 
 """Functions for converting between color spaces.
@@ -1898,7 +1898,7 @@ def separate_stains(rgb, conv_matrix, *, channel_axis=-1):
     rgb : (..., C=3, ...) array_like
         The image in RGB format. By default, the final dimension denotes
         channels.
-    conv_matrix: ndarray
+    conv_matrix : ndarray
         The stain separation matrix as described by G. Landini [1]_.
     channel_axis : int, optional
         This parameter indicates which axis of the array corresponds to
@@ -2008,7 +2008,7 @@ def combine_stains(stains, conv_matrix, *, channel_axis=-1):
     stains : (..., C=3, ...) array_like
         The image in stain color space. By default, the final dimension denotes
         channels.
-    conv_matrix: ndarray
+    conv_matrix : ndarray
         The stain separation matrix as described by G. Landini [1]_.
     channel_axis : int, optional
         This parameter indicates which axis of the array corresponds to
