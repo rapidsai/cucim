5cf9d5a6e4d4c5d4355795cc41b7aa65c441a936
Gregory R. Lee <grelee@nvidia.com>
2026-03-03 21:35:22 -0500

    add __init__.pyi to segmentation module

---
+++ python/cucim/src/cucim/skimage/segmentation/__init__.pyi
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# Algorithms to partition images into meaningful regions or boundaries
+
+import lazy_loader as lazy
+
+__getattr__, __dir__, __all__ = lazy.attach_stub(__name__, __file__)
