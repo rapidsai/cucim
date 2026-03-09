4a287c5e16714d30b189b5b1c1c3931588c4125d
Gregory R. Lee <grelee@nvidia.com>
2026-03-09 14:18:07 -0400

    copyright fix

--- python/cucim/src/cucim/skimage/restoration/__init__.pyi
+++ python/cucim/src/cucim/skimage/restoration/__init__.pyi
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2009-2022 the scikit-image team
-# SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0 AND BSD-3-Clause
 
 # Explicitly setting `__all__` is necessary for type inference engines
