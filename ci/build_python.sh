771e986fb4fbd234d3ee18b1dcc6b3d06abf8672
ptaylor <paul.e.taylor@me.com>
2026-03-31 23:35:17 -0700

    use S3 for preprocessor cache

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -18,13 +18,19 @@ rapids-generate-version > ./VERSION
 
 rapids-logger "Begin py build"
 
+sccache --stop-server 2>/dev/null || true
+
+export SCCACHE_S3_KEY_PREFIX="cucim/${RAPIDS_CONDA_ARCH}/cuda${RAPIDS_CUDA_VERSION%%.*}/object-cache"
+export SCCACHE_S3_PREPROCESSOR_CACHE_KEY_PREFIX="cucim/${RAPIDS_CONDA_ARCH}/cuda${RAPIDS_CUDA_VERSION%%.*}/preprocessor-cache"
+export SCCACHE_S3_USE_PREPROCESSOR_CACHE_MODE="true"
+
+sccache --start-server
+
 # this can be set back to 'prevent' once the xorg-* migrations are completed
 # ref: https://github.com/rapidsai/cucim/issues/800#issuecomment-2529593457
 conda config --set path_conflict warn
 
 CPP_CHANNEL=$(rapids-download-conda-from-github cpp)
-
-sccache --zero-stats
 
 # TODO: Remove `--no-test` flag once importing on a CPU
 # node works correctly
