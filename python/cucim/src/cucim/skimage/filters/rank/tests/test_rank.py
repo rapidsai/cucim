5449a1f9dbf48766ae9d49250d14a58792b21ac2
Gregory R. Lee <grelee@nvidia.com>
2026-04-09 16:23:49 -0400

    bump copyright

--- python/cucim/src/cucim/skimage/filters/rank/tests/test_rank.py
+++ python/cucim/src/cucim/skimage/filters/rank/tests/test_rank.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2009-2022 the scikit-image team
-# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0 AND BSD-3-Clause
 
 
