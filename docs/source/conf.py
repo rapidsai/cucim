e902f8beae396872cd0a0a331b6438646f7b63d4
Bradley Dice <bdice@bradleydice.com>
2026-09-01 09:24:11 -0500

    Enable public docs features in CI (#1140)
    Contributes to https://github.com/rapidsai/build-planning/issues/319
    
    Enable the Sphinx theme's `public_docs_features` option when `CI=true`,
    while leaving local documentation builds unchanged.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -109,6 +109,7 @@ todo_include_todos = False
 
 html_theme = "nvidia_sphinx_theme"
 html_theme_options = {
+    "public_docs_features": os.environ.get("CI") == "true",
     "icon_links": [
         {
             "name": "GitHub",
